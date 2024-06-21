4e6a1419292ae59e41e0352612744da0862878a3
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2024-06-21 11:09:03 +0800

    add server

---
+++ server.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+
+import config
+from main import CrawlerFactory
+from recv_sms import app
+
+
+class SearchRequest(BaseModel):
+    platform: str
+    login_type: str
+    type : str
+    start: int
+    keywords: str
+
+
+@app.post("/search")
+async def search_xsh(search_request: SearchRequest):
+    config.PLATFORM = search_request.platform
+    config.LOGIN_TYPE = search_request.login_type
+    config.CRAWLER_TYPE = search_request.type
+    config.START_PAGE = search_request.start
+    config.KEYWORDS = search_request.keywords
+    crawler = CrawlerFactory.create_crawler(platform=config.PLATFORM)
+    await crawler.start()
+    return {"status": "ok"}
+
+
+
